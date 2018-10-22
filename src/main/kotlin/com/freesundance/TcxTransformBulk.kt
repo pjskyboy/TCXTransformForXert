7d3e1351954a360664554de42467b77eca907176
pjskyboy <skyboy125>
2018-10-22 20:46:33 +0100

    Removbed dud From template - need to re-record!

--- src/main/kotlin/com/freesundance/TcxTransformBulk.kt
+++ src/main/kotlin/com/freesundance/TcxTransformBulk.kt
@@ -13,11 +13,11 @@ private val logger = KotlinLogging.logger {}
 fun main(args: Array<String>) {
         val tcxTransform = TcxTransform()
 
-        var day=19
-        while (day < 20) {
+        var day=28
+        while (day < 29) {
             logger.info("day=$day")
             val dayStr = String.format("%02d", day)
-            val dateStr = "2018-10-$dayStr"
+            val dateStr = "2018-09-$dayStr"
             tcxTransform.transform(dateStr, "TO")
             tcxTransform.transform(dateStr, "FROM")
             day++
