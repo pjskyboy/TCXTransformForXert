02a8a82e9d167a60fd8b1c74cb712ca176ae65c7
pjskyboy <skyboy125>
2018-11-01 22:25:01 +0000

    Command line args on bulk converter

--- src/main/kotlin/com/freesundance/TcxTransformBulk.kt
+++ src/main/kotlin/com/freesundance/TcxTransformBulk.kt
@@ -13,18 +13,22 @@ import javax.xml.parsers.DocumentBuilderFactory
 private val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>) {
-        val tcxTransform = TcxTransform()
-
-        var currentDate = LocalDate.parse("2018-09-03")
-        var endDate = LocalDate.parse("2018-10-26")
-        while (currentDate < endDate.plusDays(1)) {
-            logger.info("currentDate=$currentDate ${currentDate.dayOfWeek}")
-            if (isWeekday(currentDate.dayOfWeek)) {
-                tcxTransform.transform(currentDate.toString(), "TO")
-                tcxTransform.transform(currentDate.toString(), "FROM")
+    val usage = "TcxTransformBulk.kt YYYY-MM-DD YYYY-MM-DD"
+        if (args.size == 2) {
+            val tcxTransform = TcxTransform()
+            var currentDate = LocalDate.parse(args[0])
+            var endDate = LocalDate.parse(args[1])
+            while (currentDate < endDate.plusDays(1)) {
+                logger.info("currentDate=$currentDate ${currentDate.dayOfWeek}")
+                if (isWeekday(currentDate.dayOfWeek)) {
+                    tcxTransform.transform(currentDate.toString(), "TO")
+                    tcxTransform.transform(currentDate.toString(), "FROM")
+                }
+                currentDate = currentDate.plusDays(1)
             }
-            currentDate = currentDate.plusDays(1)
-    }
+        } else {
+            logger.warn(usage)
+        }
 }
 
 fun isWeekday(dayOfWeek : DayOfWeek): Boolean {
